40f7074f6a94c0d42419248bebf6d42db80c7909
Vicken <vicken.liu@gmail.com>
2016-04-01 14:27:18 +1300

    initial the page

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+
+
+var gulp = require("gulp");
+//var browserify=require('browserify');
+//var source=require('vinyl-source-stream');
+var sass=require('gulp-sass');
+
+
+gulp.task('sass',function(){
+	return gulp.src('sass/*.scss')
+			.pipe(sass())
+			.pipe(gulp.dest('css'));
+});
+
+gulp.task('watch',function(){
+	gulp.watch('sass/*.scss',['sass']);
+});
+
+gulp.task('default',['sass','watch']);
