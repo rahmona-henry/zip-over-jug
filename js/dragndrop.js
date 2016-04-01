f8caaab9ed99a0b3416a7afe5265f2ac60b093e8
Benjamin Scully & Lizzie LaShark <pair+ben-scully.lizzielashark@devbootcamp.com>
2016-04-01 15:08:58 +1300

    updated droppable functions to work with Vickens CSS

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -1,10 +1,11 @@
+// $(init) //possibly means 'on page load'
 
-$(init) //possibly means 'on page load'
+function dragNdrop() {
 
-function init() {
-
-  $('#cup').draggable({
+  $('.cup').each( function () {
+      $( this ).draggable({
     containment: '#wrapper'
+    })
   })
 
   $('#underZip').droppable({
@@ -17,19 +18,26 @@ function init() {
 
 }
 
-
-
-
 function pourTheZip(event, cup) {
-  cup.draggable.addClass( 'cupDropped' )
-  cup.draggable.position( { of: $(this), my: 'center top', at: 'center top' } )
-}
+  cup.draggable.position( { of: $(this), my: 'center bottom', at: 'center bottom' } )
+  $('#zip').addClass('run')
 
+  setTimeout( function () {
+    cup.draggable.removeClass( 'run' )
+  }, 1000)
+
+  setTimeout( function () {
+    cup.draggable.addClass( 'fill' )
+  }, 1000)
+}
 
 function dishwashCup(event, cup) {
   var cupID = "#" + cup.draggable[0].id  //need to add a # so that when it's passed into the $().remove, jquery knows it's an ID
   $(cupID).remove();
+  $('#dishwasher').addClass('open')
 }
+
+module.exports = dragNdrop
 
 
 
