f588aca26c23f990770ac9e0c21e38c55d1233d9
George Armstrong & Rawad Alawar <pair+george-of-croton.rawad-alawar@devbootcamp.com>
2016-04-01 17:47:49 +1300

    bullsheeeeit

--- js/flickr.js
+++ js/flickr.js
@@ -6,15 +6,17 @@ dotenv.load()
 var url
 
 var getPhoto = function(str, callback){
-var query = [
-  'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=',
-  process.env.FLICKR_API_KEY,
-  '&tags='+ str + '&per_page=6&format=json&nojsoncallback=1'   /////user input tag
-].join('')
+  var query = [
+    'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=',
+    process.env.FLICKR_API_KEY,
+    '&tags='+ str + '&per_page=6&format=json&nojsoncallback=1'   /////user input tag
+    ].join('')
 
-  agent.get(query, function(err, response, body){
-    if (err) {console.log("error!", err)
-      return}
+ agent.get(query, function(err, response, body){
+    if (err) {
+      console.log("error!", err)
+      return
+    }
 
     var object = JSON.parse(response.text)
     object = object.photos.photo[0]
@@ -23,10 +25,10 @@ var query = [
       getRandomWord()
     }
     else{
-    url = 'https://farm'+ object.farm +'.staticflickr.com/'+ object.server +'/' + object.id + '_' + object.secret +'_t.jpg'
-    console.log(url, "this is URL")
-    return url
-  }
+      url = 'https://farm'+ object.farm +'.staticflickr.com/'+ object.server +'/' + object.id + '_' + object.secret +'_t.jpg'
+      console.log(url, "this is URL")
+      return url
+    }
   })
 }
 
@@ -41,18 +43,20 @@ var getRandomWord = function(callback){
 
 getRandomWord()
 
+function setBackgrounds(){
+ var cups = document.querySelectorAll(".cup")
+console.log(cups)
+for(i = 0; i < cups.length; i++){
+  cups[i].style.background = "url('"+url+"')"
+}
+}
+
+
 exports = module.export = {
   getRandomWord : getRandomWord,
   getPhoto : getPhoto
 }
 
-// var rando = getRandomWord()
-// console.log(rando)
-// var query = [
-//   'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=',
-//   process.env.FLICKR_API_KEY,
-//   '&tags='+ rando + '&per_page=6&format=json&nojsoncallback=1'   /////user input tag
-// ].join('')
-// getPhoto()
+
 
 
--- js/pictureApplicator.js
+++ js/pictureApplicator.js
@@ -3,11 +3,11 @@ console.log(cups)
 for(i = 0; i < cups.length; i++){
   cups[i].style.background = "url('https://farm1.staticflickr.com/20/71287943_803b42beb6_t.jpg')"
 }
-var getRandomWord= require('./flickr');
+var getRandomWord= require('./flickr.js');
 
 function setBackgrounds(){
   for(var i=0; i<9; i++){
     var img=getRandomWord();
-    document.querySelectorAll("#cup"+i).style.background = "url('" + +"')"
+    setTimeout( function() {document.querySelectorAll("div.cup"+i).style.background = "url('" + img +"')";}, 3000);
   }
 }
